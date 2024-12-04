92a6b5594b4b70b11bd10a725acc082032088e71
Honeybunch <amt3824@g.rit.edu>
2024-12-04 10:29:13 -0800

    Trying to set up a proper loading screen pattern

---
+++ Source/HotGirlBaseBall/HotGirlBaseballGameInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Blueprint/UserWidget.h" 
+#include "HotGirlBaseballGameInstance.generated.h"
+
+/**
+ * We override the game instance to have more control over lifetimes for things like loading screens
+ */
+UCLASS()
+class HOTGIRLBASEBALL_API UHotGirlBaseballGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Init() override;
+
+	UFUNCTION()
+	virtual void BeginLoadingScreen(const FString& MapName);
+
+	UFUNCTION()
+	virtual void EndLoadingScreen(UWorld* InLoadedWorld);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UUserWidget> LoadingScreenClass;
+};
