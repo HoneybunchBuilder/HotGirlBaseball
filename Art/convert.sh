d00e79e965bf2f7b0e464d104a83c57d05fc4796
Honeybunch <amt3824@g.rit.edu>
2025-03-01 18:24:22 -0800

    Adding square icon variant

--- Art/convert.sh
+++ Art/convert.sh
@@ -9,6 +9,12 @@ magick -background none icon.svg -resize 96x96 icon96.png
 magick -background none icon.svg -resize 120x120 icon120.png
 magick -background none icon.svg -resize 152x152 icon152.png
 magick -background none icon.svg -resize 167x167 icon167.png
+magick icon-square.svg -resize 184x184 icon184-square.png
+magick icon-square.svg -resize 184x184 icon184-square.jpg
+magick icon-square.svg -resize 64x64 icon64-square.jpg
+magick icon-square.svg -resize 32x32 icon32-square.jpg
+magick icon-square.svg -resize 24x24 icon24-square.jpg
+magick icon-square.svg -resize 16x16 icon16-square.jpg
 magick -background none icon.svg -resize 256x256 icon256.png
 magick -background none icon.svg -resize 1024x1024 icon1024.png
 magick -background none icon.svg icon.ico
