8be7415bc58d49c45df13acd557737afd623911b
Honeybunch <amt3824@g.rit.edu>
2025-03-01 17:05:06 -0800

    adding a new icon

---
+++ Art/convert.sh
@@ -0,0 +1,11 @@
+magick -background none icon.svg -resize 36x36 icon36.png
+magick -background none icon.svg -resize 48x48 icon48.png
+magick -background none icon.svg -resize 72x72 icon72.png
+magick -background none icon.svg -resize 96x96 icon96.png
+magick -background none icon.svg -resize 120x120 icon120.png
+magick -background none icon.svg -resize 152x152 icon152.png
+magick -background none icon.svg -resize 167x167 icon167.png
+magick -background none icon.svg -resize 256x256 icon256.png
+magick -background none icon.svg -resize 1024x1024 icon1024.png
+magick -background none icon.svg icon.ico
+magick -background none icon.svg -resize 512x512 icon.icns
