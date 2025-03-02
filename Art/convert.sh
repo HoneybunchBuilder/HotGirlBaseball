62142d564d03dedfd5e1b405663b0514dde8fc62
Honeybunch <amt3824@g.rit.edu>
2025-03-01 17:13:52 -0800

    Cleaning up some files and updating icon conversion script

--- Art/convert.sh
+++ Art/convert.sh
@@ -1,5 +1,9 @@
+magick -background none icon.svg -resize 16x16 icon16.png
+magick -background none icon.svg -resize 24x24 icon24.png
+magick -background none icon.svg -resize 32x32 icon32.png
 magick -background none icon.svg -resize 36x36 icon36.png
 magick -background none icon.svg -resize 48x48 icon48.png
+magick -background none icon.svg -resize 64x64 icon64.png
 magick -background none icon.svg -resize 72x72 icon72.png
 magick -background none icon.svg -resize 96x96 icon96.png
 magick -background none icon.svg -resize 120x120 icon120.png
@@ -8,4 +12,6 @@ magick -background none icon.svg -resize 167x167 icon167.png
 magick -background none icon.svg -resize 256x256 icon256.png
 magick -background none icon.svg -resize 1024x1024 icon1024.png
 magick -background none icon.svg icon.ico
+magick -background none icon.svg -resize 16x16 icon16.ico
+magick -background none icon.svg -resize 32x32 icon32.ico
 magick -background none icon.svg -resize 512x512 icon.icns
