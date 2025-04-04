99cb5906cfc50045e8bb60943cd5ce75737098fa
Honeybunch <amt3824@g.rit.edu>
2025-04-03 20:05:04 -0700

    Cleaning up an old function library

--- Source/HotGirlBaseball/Private/HGBBFunctionLibrary.cpp
+++ Source/HotGirlBaseball/Private/HGBBFunctionLibrary.cpp
@@ -24,3 +24,57 @@ UWidget* UHGBBFunctionLibrary::GetFocusedWidget()
 
 	return nullptr;
 }
+
+bool UHGBBFunctionLibrary::DoesPositionGuardBase(EFieldingPosition Position, EBase Base)
+{
+	// Some roles currently don't have any responsbility to guard a base
+	// We probably want this to be more dynamic; i.e. if first base is out of commission due to some ability the right fielder could cover
+	// Also consider short stop covering second or third depending on the play
+
+	if (Position == EFieldingPosition::FirstBase && Base == EBase::First)
+	{
+		return true;
+	}
+	if (Position == EFieldingPosition::SecondBase && Base == EBase::Second)
+	{
+		return true;
+	}
+	if (Position == EFieldingPosition::ThirdBase && Base == EBase::Third)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+int32 UHGBBFunctionLibrary::GetBasePriority(EBase Base)
+{
+	switch (Base)
+	{
+	case EBase::First:
+		return 0;
+	case EBase::Second:
+		return 1;
+	case EBase::Third:
+		return 2;
+	default:
+	case EBase::Home:
+		return 3;
+	}
+}
+
+EBase UHGBBFunctionLibrary::GetBaseFromPriority(int32 Priority)
+{
+	switch (Priority)
+	{
+	case 0:
+		return EBase::First;
+	case 1:
+		return EBase::Second;
+	case 2:
+		return EBase::Third;
+	case 3:
+	default:
+		return EBase::Home;
+	}
+}
--- Source/HotGirlBaseball/Public/HGBBFunctionLibrary.h
+++ Source/HotGirlBaseball/Public/HGBBFunctionLibrary.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Components/Widget.h"
+#include "HGBBTypes.h"
 
 #include "HGBBFunctionLibrary.generated.h"
 
@@ -20,4 +21,16 @@ public:
 	// Get the currently focused widget
 	UFUNCTION(BlueprintCallable)
 	static UWidget* GetFocusedWidget();
+
+	// Look up if the given position is currently responsible for the current base
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	static bool DoesPositionGuardBase(EFieldingPosition Position, EBase Base);
+
+	// Get priority order from base
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	static int32 GetBasePriority(EBase Base);
+
+	// Get base from priority order
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	static EBase GetBaseFromPriority(int32 Priority);
 };
