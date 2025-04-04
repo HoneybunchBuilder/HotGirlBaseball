c54258a450eb18b166d27dfda97bacd5922a26f7
Honeybunch <amt3824@g.rit.edu>
2025-04-03 17:37:00 -0700

    Updating some enums and fixing the Base enum name

--- Source/HotGirlBaseball/Public/HGBBTypes.h
+++ Source/HotGirlBaseball/Public/HGBBTypes.h
@@ -7,7 +7,7 @@
 #include "HGBBTypes.generated.h"
 
 UENUM(BlueprintType)
-enum class Base : uint8 {
+enum class EBase : uint8 {
     Home = 0 UMETA(DisplayName = "Home"),
     First = 1 UMETA(DisplayName = "First"),
     Second = 2 UMETA(DisplayName = "Second"),
@@ -16,7 +16,15 @@ enum class Base : uint8 {
 
 UENUM(BlueprintType)
 enum class EFieldingPosition : uint8 {
-    FirstBase = 0 UMETA(DisplayName = "First Base"),
+    Pitcher = 0 UMETA(DisplayName = "Pitcher"),
+    Catcher = 1 UMETA(DisplayName = "Catcher"),
+    FirstBase = 2 UMETA(DisplayName = "First Base"),
+    SecondBase = 3 UMETA(DisplayName = "Second Base"),
+    Shortstop = 4 UMETA(DisplayName = "Shortstop"),
+    ThirdBase = 5 UMETA(DisplayName = "Third Base"),
+    LeftField = 6 UMETA(DisplayName = "Left Field"),
+    CenterField = 7 UMETA(DisplayName = "Center Field"),
+    RightField = 8 UMETA(DisplayName = "Right Field"),
 };
 
 UENUM(BlueprintType)
@@ -28,11 +36,17 @@ enum class EHandedness : uint8 {
 UENUM(BlueprintType)
 enum class EHitArc : uint8 {
     GroundBall = 0 UMETA(DisplayName = "Ground Ball"),
+    LineDrive = 1 UMETA(DisplayName = "Line Drive"),
+    PopFly = 2 UMETA(DisplayName = "Pop Fly"),
 };
 
 UENUM(BlueprintType)
 enum class EHitDirection : uint8 {
-    LeftField = 0 UMETA(DisplayName = "Left Field"),
+    Left = 0 UMETA(DisplayName = "Left"),
+    Center = 1 UMETA(DisplayName = "Center"),
+    Right = 2 UMETA(DisplayName = "Right"),
+    Foul = 3 UMETA(DisplayName = "Foul"),
+    HomeRun = 4 UMETA(DisplayName = "Home Run"),
 };
 
 UENUM(BlueprintType)
